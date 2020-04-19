d959c5c61c9255decc0328ae64d7d21f9049c44b
Akramjon2658 <akramjon_abdullayev@mail.ru>
2020-04-19 07:27:48 +0500

    Updates

---
+++ precache-manifest.bfe8ff593b1b565d672ecc62c59981a9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f2b4d032a09d37027ba992b992d43c",
+    "url": "/mySocialNetwork/index.html"
+  },
+  {
+    "revision": "c12681dc4d7d763062e0",
+    "url": "/mySocialNetwork/static/css/3.a5d49d17.chunk.css"
+  },
+  {
+    "revision": "99dae22fa90e2925c23a",
+    "url": "/mySocialNetwork/static/css/4.0e7d05c8.chunk.css"
+  },
+  {
+    "revision": "75d08f5f17fa521bf658",
+    "url": "/mySocialNetwork/static/css/main.d0c38b7c.chunk.css"
+  },
+  {
+    "revision": "74c5dd87bf11bd9aea10",
+    "url": "/mySocialNetwork/static/js/2.7f1d0fad.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/mySocialNetwork/static/js/2.7f1d0fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12681dc4d7d763062e0",
+    "url": "/mySocialNetwork/static/js/3.6dc4d894.chunk.js"
+  },
+  {
+    "revision": "99dae22fa90e2925c23a",
+    "url": "/mySocialNetwork/static/js/4.9e500b9a.chunk.js"
+  },
+  {
+    "revision": "75d08f5f17fa521bf658",
+    "url": "/mySocialNetwork/static/js/main.00b58cdc.chunk.js"
+  },
+  {
+    "revision": "d151bcec2777c00c6b41",
+    "url": "/mySocialNetwork/static/js/runtime-main.af04b76b.js"
+  },
+  {
+    "revision": "696a389a38c317c910001d862a681b3c",
+    "url": "/mySocialNetwork/static/media/Blocks-1s-200px.696a389a.svg"
+  },
+  {
+    "revision": "23591a4b564fc5ab5f83f6c41155c27f",
+    "url": "/mySocialNetwork/static/media/IslamicBanking.23591a4b.jpg"
+  }
+]);
